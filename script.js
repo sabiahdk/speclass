b88a6abd8b67f242d55396e3545efe506e989a8b
sabiahdk <sabiahmuhd98@gmail.com>
2025-06-13 16:43:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// document.getElementById("head")
+// document.getElementById('head').style.color='blue'
+
+let target = document.getElementById('head')
+  target.style.color='yellow'
+  target.style.fontSize='50px'
