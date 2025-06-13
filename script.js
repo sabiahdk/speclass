b4630f7b6219c02f81b78a9f3b5e93f46a056662
sabiahdk <sabiahmuhd98@gmail.com>
2025-06-13 23:44:55 +0100

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // document.getElementById("head")
 // document.getElementById('head').style.color='blue'
 
-let target = document.getElementById('head')
-  target.style.color='yellow'
-  target.style.fontSize='50px'
+// let target = document.getElementById('head')
+//   target.style.color='yellow'
+//   target.style.fontSize='50px'
+
+
+let admittedPatient = 30;
+console.log(admittedPatient)
+
+admittedPatient += 10;
+console.log(admittedPatient)
+
+admittedPatient -= 15;
+console.log(admittedPatient);
+
+admittedPatient *= 2;
+console.log(admittedPatient);
+
+admittedPatient /= 5;
+console.log(admittedPatient)
+
+ admittedPatient %= 3;
+ console.log(admittedPatient);
+   
+ admittedPatient ++;
+ console.log(admittedPatient)
